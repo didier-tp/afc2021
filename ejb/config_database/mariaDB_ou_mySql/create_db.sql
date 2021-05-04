dfaac36ab1e9cd956c0f89e7c4cea6a9ebafe4d2
didier <didier@d-defrance.fr>
2021-05-04 16:07:22 +0200

    script db

--- ejb/config_database/mariaDB_ou_mySql/create_db.sql
+++ ejb/config_database/mariaDB_ou_mySql/create_db.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS myDB;
 USE myDB;
 
-
+DROP TABLE IF EXISTS operation;
 DROP TABLE IF EXISTS compte;
-
 
 
 CREATE TABLE compte(
@@ -12,5 +11,15 @@ CREATE TABLE compte(
 	label VARCHAR(64),
 	solde double,
 	PRIMARY KEY(numero));
+	
+CREATE TABLE operation(
+	label VARCHAR(64),
+	montant double,
+	numOp integer auto_increment,
+	dateOp DATE,
+	numCompte integer,
+	PRIMARY KEY(numOp));		
 
+ALTER TABLE operation ADD CONSTRAINT Operation_avec_compte_valide 
+FOREIGN KEY (numCompte) REFERENCES compte(numero);
 	
--- ejb/config_database/mariaDB_ou_mySql/insert_db.sql
+++ ejb/config_database/mariaDB_ou_mySql/insert_db.sql
@@ -4,6 +4,7 @@ USE myDB;
 
 #########################  INSERT INTO   #####################################
 
+DELETE FROM operation;
 DELETE FROM compte;
 
 INSERT INTO compte(numero,label,solde) 
@@ -12,11 +13,14 @@ INSERT INTO compte(numero,label,solde)
      VALUES (2,'compte 2',50);
 INSERT INTO compte(numero,label,solde) 
      VALUES (3,'compte 3',20);
+	 
+INSERT INTO operation (dateOp,label,montant,numOp,numCompte)  VALUES ('2011-01-20','achat yy',-50.0,1,1);
+INSERT INTO operation (dateOp,label,montant,numOp,numCompte)  VALUES ('2011-01-21','achat zz',-30.0,2,1);	 
 
 ###################### VERIFICATIONS ###########################################
 show tables;
 SELECT * FROM compte;
-
+SELECT * FROM operation;
 
 
 
