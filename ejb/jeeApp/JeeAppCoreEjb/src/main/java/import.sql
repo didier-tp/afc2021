dd3992bd8d230b458f06defc1d0159a71d2ef68c
didier <didier@d-defrance.fr>
2021-05-04 10:19:59 +0200

    import.sql

---
+++ ejb/jeeApp/JeeAppCoreEjb/src/main/java/import.sql
@@ -0,0 +1,8 @@
+//import.sql est un script SQL automatiquement déclenché par hibernate 
+//au démarrage de appli (ou test) en mode hibernate.hbm2ddl.auto=create  
+
+
+INSERT INTO Compte(numero,label,solde) VALUES (1,'compte courant 1', 500.1) 
+INSERT INTO Compte(numero,label,solde) VALUES (2,'compte epargne 2', 300.6) 
+INSERT INTO Compte(numero,label,solde) VALUES (3,'compte courant 3', 200.1) 
+INSERT INTO Compte(numero,label,solde) VALUES (4,'compte epargne 4', 800.6) 
