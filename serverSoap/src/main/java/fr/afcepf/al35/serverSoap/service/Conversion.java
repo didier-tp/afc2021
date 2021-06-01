9fde48b34b2553b9722b1ba81c5758c4892c9644
didier <didier@d-defrance.fr>
2021-06-01 12:20:38 +0200

    s

--- serverSoap/src/main/java/fr/afcepf/al35/serverSoap/service/Conversion.java
+++ serverSoap/src/main/java/fr/afcepf/al35/serverSoap/service/Conversion.java
@@ -1,7 +1,13 @@
 package fr.afcepf.al35.serverSoap.service;
 
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService //pour WebService SOAP
 public interface Conversion {
-    double convertir(double montant,String codeDeviseSource,String codeDeviseCible);
+    double convertir(@WebParam(name="montant")double montant,
+    		@WebParam(name="codeDeviseSource")String codeDeviseSource,
+    		@WebParam(name="codeDeviseCible")String codeDeviseCible);
     public String getDev();
     //...
 }
