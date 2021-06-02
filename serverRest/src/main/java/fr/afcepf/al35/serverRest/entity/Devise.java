b0b99631ca9c467d5314cfbfdf1453587328669b
didier <didier@d-defrance.fr>
2021-06-02 17:30:15 +0200

    jsonIgnore

--- serverRest/src/main/java/fr/afcepf/al35/serverRest/entity/Devise.java
+++ serverRest/src/main/java/fr/afcepf/al35/serverRest/entity/Devise.java
@@ -1,13 +1,18 @@
 package fr.afcepf.al35.serverRest.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +33,11 @@ public class Devise {
 	@Min(0)
 	@Max(2345)
 	private Double change;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="devise")
+	public List<Pays> pays;
+	
 	
 	public Devise(String code, String nom, Double change) {
 		super();
