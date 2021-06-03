cef1ab4fc6cd684063a56f7ed456fc4255e70fb4
didier <didier@d-defrance.fr>
2021-06-03 11:52:46 +0200

    appel ajax dans serverRest

--- serverRest/src/main/resources/static/js/conversion-ajax.js
+++ serverRest/src/main/resources/static/js/conversion-ajax.js
@@ -1,3 +1,25 @@
 function doConversion(){
-	console.log("doConversion() appelee");
+	
+	var montant = Number(document.getElementById("txtMontant").value);
+	var deviseSource = document.getElementById("txtDeviseSource").value;
+	var deviseCible = document.getElementById("txtDeviseCible").value;
+	console.log("doConversion() appelee avec montant= " + montant);
+	
+	var url = "./devise-api-rest/devise/conversion?amount="
+	            +montant+"&source="+deviseSource+
+                "&target="+deviseCible;
+
+    var callback = function(data){
+	   console.log("sucess data=" + data);
+       var resConv = JSON.parse(data);
+       var montantConverti = resConv.result;
+       document.getElementById("spanRes").innerHTML="<b>"+montantConverti+"</b>";
+    }
+
+    var errCallback = function(data){
+	   console.log("erreur=" + data);
+    }
+
+	makeAjaxGetRequest(url,callback,errCallback) ;
+	
 }
