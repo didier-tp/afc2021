5d7c64800b94e05844a623c5f810d3528b0d706c
didier <didier@d-defrance.fr>
2021-06-03 12:24:34 +0200

    ajoutDevise ajax

--- serverRest/src/main/resources/static/js/conversion-ajax.js
+++ serverRest/src/main/resources/static/js/conversion-ajax.js
@@ -23,3 +23,33 @@ function doConversion(){
 	makeAjaxGetRequest(url,callback,errCallback) ;
 	
 }
+
+
+function doAjout(){
+	
+	var change = Number(document.getElementById("txtChange").value);
+	var code = document.getElementById("txtCode").value;
+	var nom = document.getElementById("txtNom").value;
+
+	
+	var url = "./devise-api-rest/devise"
+
+    var callback = function(data){
+	   console.log("sucess data=" + data);
+       var message ="donnees sauvegardees cote serveur=" + data;
+       document.getElementById("spanMessage").innerHTML="<b>"+message+"</b>";
+    }
+
+    var errCallback = function(data){
+	   console.log("erreur=" + data);
+       var message = (JSON.parse(data)).message;
+       document.getElementById("spanMessage").innerHTML="<b>"+message+"</b>";
+    }
+
+    //var jsDeviseObject = {code : code , nom ; nom , change : change };
+    var jsDeviseObject = {code , nom , change };
+    var jsonData = JSON.stringify(jsDeviseObject);
+
+	makeAjaxPostRequest(url,jsonData,callback,errCallback) ;
+	
+}
