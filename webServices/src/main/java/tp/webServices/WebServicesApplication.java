04516a35c34f3fcde935ad5fc905981816e8777e
didierDefrance <didier@d-defrance.fr>
2021-12-15 09:51:38 +0100

    s

--- webServices/src/main/java/tp/webServices/WebServicesApplication.java
+++ webServices/src/main/java/tp/webServices/WebServicesApplication.java
@@ -2,12 +2,18 @@ package tp.webServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebServicesApplication.class, args);
+		//SpringApplication.run(WebServicesApplication.class, args);
+		
+		SpringApplication app = new SpringApplication(WebServicesApplication.class);
+		app.setAdditionalProfiles("initDataSet"/*,"swagger"*/);
+		ConfigurableApplicationContext context = app.run(args);
+		
 		System.out.println("http://localhost:8484/webServices/index.html");
 	}
 
