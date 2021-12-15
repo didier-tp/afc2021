4ef970d0ecbfff9cf7aa929062075f698683ade6
didierDefrance <didier@d-defrance.fr>
2021-12-15 11:38:38 +0100

    s

--- webServices/src/main/java/tp/webServices/rest/DeviseRestCtrl.java
+++ webServices/src/main/java/tp/webServices/rest/DeviseRestCtrl.java
@@ -2,8 +2,12 @@ package tp.webServices.rest;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +39,18 @@ public class DeviseRestCtrl {
 	public List<Devise> getDevisesByCriteria(){
 	    return deviseService.rechercherToutesDevises();
 	}
+	
+	//POST
+	//http://localhost:8484/webServices/devise-api/private/devise
+	//avec { "code" : "DDK" , "nom" : "couronne danoise" , "change" : 7.77 }
+	//dans partie body de la requête entrante
+	@PostMapping(value="/private/devise" )
+	public ResponseEntity<Devise> postDevise(@RequestBody Devise d){
+		   //v1 (à beaucoup améliorer)
+		   deviseService.sauvegarderDevise(d);
+		   return new ResponseEntity<Devise>(d, HttpStatus.OK);
+	}
+	
+	
 
 }
